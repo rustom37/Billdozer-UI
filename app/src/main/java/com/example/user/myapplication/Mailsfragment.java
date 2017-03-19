0755642fef35e3652c7fcb99442fbf825ac8806e
jackazadian <jackelitemw3@gmail.com>
2017-03-19 22:43:58 +0200

    added add activity to the app

---
+++ app/src/main/java/com/example/user/myapplication/Mailsfragment.java
@@ -0,0 +1,21 @@
+package com.example.user.myapplication;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by User on 3/18/2017.
+ */
+
+public class Mailsfragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.mails,container,false);
+        return v;
+    }
+}
